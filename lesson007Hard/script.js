c76bb245f30542f81235d42ba88f27ce06238a9d
source <dean-91@mail.ru>
2020-08-18 16:23:07 +0300

    Обновил дз

--- lesson007Hard/script.js
+++ lesson007Hard/script.js
@@ -1,23 +1,23 @@
 'use strict';
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-   toDay = new Date().getDay() - 1;
 
-for (let i = 0; i < week.length; i++) {
-   if (i == toDay) {
-      if (week[i] == 'Cуббота' || week[i] == 'Воскресенье') {
-         document.write(`<p><b><i>${week[i]}</i></b></p>`);
-      } else {
-         document.write(`<p><b>${week[i]}</b></p>`);
-      }
-   } else if (week[i] == 'Суббота' || week[i] == 'Воскресенье') {
-      document.write(`<p><b>${week[i]}</b></p>`);
-   } else {
-      document.write(`<p>${week[i]}</p>`);
-   }
-}
-console.log(week);
+let day = document.querySelectorAll('.day');
+let dayList = document.querySelector('.day-list');
 
+let date = new Date();
+let today = date.getDay();
 
+let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+let bustOfDay = function () {
+   day.forEach((item, index, arr) => {
+      item.textContent = week[index];
+      arr[0].style.fontStyle = 'italic';
+      arr[arr.length - 1].style.fontStyle = 'italic';
+      arr[today].style.fontWeight = '800';
+   });
+};
+
+bustOfDay();
 //Метод getDay() возвращает порядковый номер дня недели указанной даты по местному времени, где 0 соответствует воскресенью.
 //Методы document.write и document.writeln пишут напрямую в текст документа, до того как браузер построит из него DOM, поэтому они могут записать в документ все, что угодно, любые стили и незакрытые теги.
 //Обратные кавычки же имеют «расширенную функциональность». Они позволяют нам встраивать выражения в строку, заключая их в ${…}
