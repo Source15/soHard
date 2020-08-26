e41c21a3e66628d75dba0557334a9f8822702324
source <dean-91@mail.ru>
2020-08-26 19:24:07 +0300

    первая часть 14го урока

--- hard/script.js
+++ hard/script.js
@@ -1,8 +1,41 @@
-const setBg = () => {
-   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-   document.body.style.backgroundColor = "#" + randomColor;
-   color.innerHTML = "#" + randomColor;
-}
+'use strict';
 
-genNew.addEventListener("click", setBg);
-setBg();
+const DomElement = function (selector, height, width, bg, fonSize) {
+   this.selector = selector;
+   this.height = height;
+   this.width = width;
+   this.bg = bg;
+   this.fonSize = fonSize;
+};
+
+DomElement.prototype.createElem = function (txt) {
+   if (this.selector[0] === '.') {
+      const div = document.createElement('div');
+      let newClass = this.selector.split('.')[1];
+      div.classList.add(newClass);
+      div.style.cssText = `
+      height:${this.height};
+      width:${this.width};
+      background:${this.bg};
+      font-size:${this.fonSize};
+      ;`
+      div.textContent = txt;
+      document.body.append(div);
+   } else if (this.selector[0] === '#') {
+      const paragraph = document.createElement('p');
+      let newId = this.selector.split('#')[1];
+      paragraph.setAttribute('id', newId);
+      paragraph.style.cssText = `
+      height:${this.height};
+      width:${this.width};
+      background:${this.bg};
+      font-size:${this.fonSize};
+      ;`
+      paragraph.textContent = txt;
+      document.body.append(paragraph);
+   }
+};
+
+const obj = new DomElement('.thisblovk', '300px', '300px', '#9cd3d9', '90px');
+
+obj.createElem('Привет мир!');
